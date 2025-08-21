e5353040f9ee387a1dbf2544e1f1a8b92e995824
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-08-21 15:45:39 -0700

    Update dependencies

--- Sources/Container-Compose/Application.swift
+++ Sources/Container-Compose/Application.swift
@@ -6,9 +6,8 @@
 //
 
 import Foundation
-import Yams
 import ArgumentParser
-import ContainerCLI
+import ComposeCLI
 
 @main
 struct Application: AsyncParsableCommand {
@@ -16,19 +15,7 @@ struct Application: AsyncParsableCommand {
         commandName: "container-compose",
         abstract: "A tool to use manage Docker Compose files with Apple Container",
         subcommands: [
-            ContainerCLI.Application.ComposeUp.self,
-            ContainerCLI.Application.ComposeDown.self
+            ComposeCLI.ComposeUp.self,
+            ComposeCLI.ComposeDown.self
         ])
-}
-
-/// A structure representing the result of a command-line process execution.
-struct CommandResult {
-    /// The standard output captured from the process.
-    let stdout: String
-
-    /// The standard error output captured from the process.
-    let stderr: String
-
-    /// The exit code returned by the process upon termination.
-    let exitCode: Int32
 }
