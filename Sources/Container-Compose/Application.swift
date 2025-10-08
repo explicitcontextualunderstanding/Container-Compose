48e90b713dcaec681fe8b1f3d26596829210362f
Andrew Basson <andrew.basson@gmail.com>
2025-10-08 16:33:34 +0200

    fix: add version command and arg (#9)
    * fix: add version command and arg
    
    Signed-off-by: Andrew Basson <andrew.basson@gmail.com>
    
    * fix: standardise the output of version and --version
    
    Signed-off-by: Andrew Basson <andrew.basson@gmail.com>
    
    * fix: scope version strings
    
    Signed-off-by: Andrew Basson <andrew.basson@gmail.com>
    
    * fix: scope version command to internal
    
    Signed-off-by: Andrew Basson <andrew.basson@gmail.com>
    
    * fix: cleanup more scopes
    
    Signed-off-by: Andrew Basson <andrew.basson@gmail.com>
    
    ---------
    
    Signed-off-by: Andrew Basson <andrew.basson@gmail.com>

--- Sources/Container-Compose/Application.swift
+++ Sources/Container-Compose/Application.swift
@@ -10,11 +10,18 @@ import ArgumentParser
 
 @main
 struct Main: AsyncParsableCommand {
+    private static let commandName: String = "container-compose"
+    private static let version: String = "v0.5.1"
+    static var versionString: String {
+        "\(commandName) version \(version)"
+    }
     static let configuration: CommandConfiguration = .init(
-        commandName: "container-compose",
+        commandName: Self.commandName,
         abstract: "A tool to use manage Docker Compose files with Apple Container",
+        version: Self.versionString,
         subcommands: [
             ComposeUp.self,
-            ComposeDown.self
+            ComposeDown.self,
+            Version.self
         ])
 }
