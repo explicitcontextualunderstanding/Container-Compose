9f5068459ac332cc0066482d337c8b986f9a3c3b
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-11-20 10:41:49 -0800

    update version to v0.6.1

--- Sources/Container-Compose/Application.swift
+++ Sources/Container-Compose/Application.swift
@@ -19,7 +19,7 @@ import ArgumentParser
 
 public struct Main: AsyncParsableCommand {
     private static let commandName: String = "container-compose"
-    private static let version: String = "v0.6.0"
+    private static let version: String = "v0.6.1"
     public static var versionString: String {
         "\(commandName) version \(version)"
     }
